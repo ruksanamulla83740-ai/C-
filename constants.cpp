0b80cfb40f350f9c7e53839b678067124b850c67
ruksanamulla83740-ai <ruksanamulla83740@gmail.com>
2026-03-31 00:34:31 +0530

    Add files via upload

---
+++ constants.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    const float a = 3.11;
+    cout<<"The value of a was: "<<a<<endl;
+   // a = 45.67;
+    //cout<<"The value of a is: "<<a<<endl;
+    //This value shows error because we have declare the a value with constant so after decalaring it the value a will be doesn't works.
+    return 0;
+
+}
